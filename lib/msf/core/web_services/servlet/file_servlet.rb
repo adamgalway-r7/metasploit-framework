94806cb995567dbc8b6de6f04a8d73573689cd50
Adam Galway <adam_galway@rapid7.com>
2020-06-10 13:07:06 +0100

    sets helper methods as private

--- lib/msf/core/web_services/servlet/file_servlet.rb
+++ lib/msf/core/web_services/servlet/file_servlet.rb
@@ -214,36 +214,38 @@ module FileServlet
       set_json_data_response(response: result)
     }
   end
-end
 
-def list_local_path(path)
-  # Enumerate each item...
-  tbl = []
-  files = Dir.entries(path)
-  files.each do |file|
-    file_path = File.join(path, file)
-    stat = File.stat(file_path)
-    row = {
-      name: file,
-      type: stat.ftype || '',
-      size: stat.size ? stat.size.to_s : '',
-      last_modified: stat.mtime || ''
-    }
-    next unless file != '.' && file != '..'
+  private
 
-    tbl << row
+  def list_local_path(path)
+    # Enumerate each item...
+    tbl = []
+    files = Dir.entries(path)
+    files.each do |file|
+      file_path = File.join(path, file)
+      stat = File.stat(file_path)
+      row = {
+        name: file,
+        type: stat.ftype || '',
+        size: stat.size ? stat.size.to_s : '',
+        last_modified: stat.mtime || ''
+      }
+      next unless file != '.' && file != '..'
+
+      tbl << row
+    end
+    return tbl
   end
-  return tbl
-end
 
-def safe_expand_path?(path)
-  current_directory = File.expand_path(Msf::Config.rest_files_directory) + File::SEPARATOR
-  tested_path = File.expand_path(path) + File::SEPARATOR
-  tested_path.starts_with?(current_directory)
-end
+  def safe_expand_path?(path)
+    current_directory = File.expand_path(Msf::Config.rest_files_directory) + File::SEPARATOR
+    tested_path = File.xpand_path(path) + File::SEPARATOR
+    tested_path.starts_with?(current_directory)
+  end
 
-def rest_files_directory?(path)
-  tested_path = File.expand_path(path) + File::SEPARATOR
-  current_directory = File.expand_path(Msf::Config.rest_files_directory) + File::SEPARATOR
-  tested_path == current_directory
+  def rest_files_directory?(path)
+    tested_path = File.expand_path(path) + File::SEPARATOR
+    current_directory = File.expand_path(Msf::Config.rest_files_directory) + File::SEPARATOR
+    tested_path == current_directory
+  end
 end
